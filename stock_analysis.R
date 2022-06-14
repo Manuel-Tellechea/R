9eeb440781e8170573e1548adec406dce847f131
Manuel-Tellechea <tellechea19@gmail.com>
2022-06-14 18:00:50 +0200

    updated

--- stock_analysis.R
+++ stock_analysis.R
@@ -6,4 +6,3 @@ data.AMZN <- read.csv(file = "Data/AMZN.csv", header = TRUE)
 date <- as.Date(data.AMZN$Date, format = "%Y-%m-%d")
 data.AMZN <- cbind(date, data.AMZN[,-1])
 data.AMZN <- data.AMZN[order(data.AMZN$date),]
-head(data.AMZN)
